e4e451dc47baf98d77ca3fc6fe7752bb8cd727d5
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-24 09:46:17 -0600

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Guillermo Ruiz Baires on 24/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
