a1bf39e611f171677222814a19503d77cc2fe67a
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-24 12:59:33 -0600

    Conditionals modifiers

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var useRedText = false
+    
     var body: some View {
-        Text("Hello Mom!")
-            .padding()
-            .background(.red)
-            .padding()
-            .background(.yellow)
-            .padding()
-            .background(.green)
+        // We can use this
+//        if useRedText {
+//               Button("Hello World") {
+//                   useRedText.toggle()
+//               }
+//               .foregroundColor(.red)
+//           } else {
+//               Button("Hello World") {
+//                   useRedText.toggle()
+//               }
+//               .foregroundColor(.blue)
+//           }
+        // but it's better use a ternary operator
+        
+        Button("Hello World") {
+            useRedText.toggle()
+        }
+        .foregroundColor(useRedText ? .red : .blue)
     }
 }
 
