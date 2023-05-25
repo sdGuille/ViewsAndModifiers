467df7f6c6d11ecccda254a769cd2cbebdcb11be
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-25 14:18:35 -0600

    View composition

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,28 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    let motto1 = Text("Draco dormiens")
-    let motto2 = Text("nunquam titillandus")
-    
-    @ViewBuilder var spells: some View {
-        Text("Lumos")
-        Text("Obliviate")
+
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+                .foregroundColor(.yellow)
+            CapsuleText(text: "Second")
+                .foregroundColor(.red)
+            CapsuleText(text: "Third")
+                .foregroundColor(.green)
+        }
     }
+}
+
+struct CapsuleText: View {
+    var text: String
     
     var body: some View {
-        
-        VStack {
-            motto1
-                .foregroundColor(.blue)
-            motto2
-                .foregroundColor(.red)
-            spells
-                .foregroundColor(.brown)
-        }
-        
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .background(.blue)
+            .clipShape(Capsule())
     }
-    
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
