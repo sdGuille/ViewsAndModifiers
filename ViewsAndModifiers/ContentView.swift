0f8113d240cf207124b184d6b8da1dfd361ba933
Guillermo Ruiz <guillermoruizbaires@gmail.com>
2023-05-25 14:42:51 -0600

    Custom modifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,16 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
 
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.red)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+
+struct ContentView: View {
+    
     var body: some View {
         VStack(spacing: 10) {
             CapsuleText(text: "First")
                 .foregroundColor(.yellow)
-            CapsuleText(text: "Second")
-                .foregroundColor(.red)
-            CapsuleText(text: "Third")
-                .foregroundColor(.green)
+            Text("Hello Mom!")
+                .titleStyle()
+            Color.blue
+                .frame(width: 300, height: 200)
+                .watermarked(with: "©copyright sdGuille")
         }
     }
 }
